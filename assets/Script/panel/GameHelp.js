811863745577cbf93de9b7a60a2ffd42f78eaea5
libz <libingzhi2011@163.com>
2023-03-30 12:41:32 +0800

    chore: upgrade creator to 2.3.4

--- assets/Script/panel/GameHelp.js
+++ assets/Script/panel/GameHelp.js
@@ -1,23 +1,23 @@
-var GameConfig = require("GameConfig");
-var GameTools = require("GameTools");
-var GameUiTools = require("GameUiTools");
+const GameConfig = require('GameConfig')
+const GameTools = require('GameTools')
+const GameUiTools = require('GameUiTools')
 cc.Class({
-    extends: cc.Component,
-    properties: {
-        backColor: cc.Node,
-        backButton: cc.Node, //返回按钮
-    },
+  extends: cc.Component,
+  properties: {
+    backColor: cc.Node,
+    backButton: cc.Node // 返回按钮
+  },
 
-    onLoad() {
-        GameUiTools.setButtonClickEvents(this, this.backButton, "buttonFunc");
-    },
+  onLoad () {
+    GameUiTools.setButtonClickEvents(this, this.backButton, 'buttonFunc')
+  },
 
-    buttonFunc: function (event) {
-        let button = event.target;
-        if (this.backButton == button) {
-            GameTools.playSimpleAudioEngine(0);
-            this.node.destroy();
-        }
-        return true;
-    },
-});
+  buttonFunc: function (event) {
+    const button = event.target
+    if (this.backButton == button) {
+      GameTools.playSimpleAudioEngine(0)
+      this.node.destroy()
+    }
+    return true
+  }
+})
